14bb8688a554fb3eafb3d0e12a621f19e1c9efac
Mishel Tyurkova <misheltyurkova@gmail.com>
2023-06-03 07:41:43 +1000

    commit tree d143385766f8fea68011c4e639f686d7994813e2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    createToDo(e.target.newTaskDescription.value)
+    form.reset()
+  });
 });
+
+function createToDo(toDo) {
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'X'
+  p.textContent = `${toDo} `
+  p.appendChild(btn)
+  document.querySelector('#list').appendChild(p)
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove()
+}
